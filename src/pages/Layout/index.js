09e8c05cf8470988d0a5a0f4e7c07434da885c8c
small-cat-hub <207336737+small-cat-hub@users.noreply.github.com.>
2025-10-24 18:08:30 +0800

    Initialize the project

---
+++ src/pages/Layout/index.js
@@ -0,0 +1,9 @@
+const Layout = () => {
+    return (
+        <div>
+            这是页面
+        </div>
+    )
+}
+
+export default Layout
---
+++ src/pages/Login/index.js
@@ -0,0 +1,9 @@
+const Login = () => {
+    return (
+        <div>
+            这是登录页面
+        </div>
+    )
+}
+
+export default Login
