693a04b8332f7f8888a601c7e63b2cd1c83313d4
small-cat-hub <207336737+small-cat-hub@users.noreply.github.com.>
2025-10-24 22:19:16 +0800

    配置二级路由

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 // 路由配置
 import Layout from "@/pages/Layout";
 import Login from "@/pages/Login";
+import Article from "@/pages/Article";
+import Publish from "@/pages/Publish";
 import { createBrowserRouter } from 'react-router-dom'
 import { AuthRoute } from '@/components/AuthRoute'
 
@@ -9,7 +11,20 @@ import { AuthRoute } from '@/components/AuthRoute'
 const router = createBrowserRouter([
     {
         path: '/',
-        element: <AuthRoute><Layout /></AuthRoute>
+        element: <AuthRoute><Layout /></AuthRoute>,
+        children:[
+            {
+                index: true
+            },
+            {
+                path:'article',
+                element: <Article />
+            },
+            {
+                path:'publish',
+                element: <Publish />
+            }
+        ]
     },
     {
         path: '/login',
