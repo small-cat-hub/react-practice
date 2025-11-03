e2d2e0b91937f59b391e9bfd7df21a2cf83c481a
small-cat-hub <207336737+small-cat-hub@users.noreply.github.com.>
2025-11-03 18:36:42 +0800

    图标初始化渲染

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,7 +1,35 @@
+import * as echarts from 'echarts';
+import { useEffect } from 'react';
+
 const Home = () => {
+    useEffect(()=>{
+        // 保证DOM可用 才进行图标的渲染
+        // 获取渲染图表的DOM节点
+        const chartDom = document.getElementById('main');
+        // 图标初始化生成图标实例对象
+        const myChart = echarts.init(chartDom);
+        // 准备图标参数
+        const option = {
+          xAxis: {
+            type: 'category',
+            data: ['vue','react','angular']
+          },
+          yAxis: {
+            type: 'value'
+          },
+          series: [
+            {
+              data: [10,40,70],
+              type: 'bar'
+            }
+            ]
+        };
+        // 使用图标参数完成渲染
+        option && myChart.setOption(option);
+    },[])
     return (
         <div>
-            Home
+            <div id='main' style={{ width: '500px', height: '400px' }}></div>
         </div>
     )
 }
